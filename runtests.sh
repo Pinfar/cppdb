80c32f9d710906bf96e45138635a0ad5fa829997
Pinfar <wilczynski.pgs@gmail.com>
2026-06-20 08:57:46 +0200

    Improved configuration.

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,11 @@
-# rm -rf build
-# cmake -S . -B build
+#!/bin/bash
 set -e
-cmake --build build
-ctest --test-dir ./build
+
+# 1. Generowanie plików projektu (jeśli folder 'build' nie istnieje lub się zmienił)
+cmake -B build
+
+# 2. Kompilacja wielowątkowa (wykorzystaj pełną moc procesora w WSL)
+cmake --build build --parallel $(nproc)
+
+# 3. Uruchomienie testów z wyświetlaniem błędów na żywo
+ctest --test-dir build --output-on-failure
